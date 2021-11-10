126464225372300707afbba11297eb5a646557c9
AWESOMESAUCE170 <55606249+AWESOMESAUCE170@users.noreply.github.com>
2021-11-10 14:02:20 -0600

    Getting windows and associated loops work as intended

--- GetDepthMap.m
+++ GetDepthMap.m
@@ -1,6 +1,7 @@
 function depthMap = GetDepthMap(firstImage, secondImage, hw, dispRange)
 %returns the depth map from a basic stareo match between two images
 %THE IMAGES MUST ALREADY HAVE BEEN RECTIFIED
+windowSize = hw * 2 + 1;
 
 %get grayscales and initialize depthMap
 firstImMat = imread(firstImage);
@@ -9,35 +10,45 @@ secondImMat = imread(secondImage);
 secondGrayMat = im2gray(secondImMat);
 imDimensions = size(firstGrayMat);
 paddedFirstImage = zeros(imDimensions(1) + 2 * hw, imDimensions(2) + 2 * hw);
-paddedFirstImage(hw + 1 : imDimensions(1) + hw + 1, hw + 1 : imDimensions(2) + hw + 1) = firstGrayMat;
+paddedFirstImage(hw + 1 : imDimensions(1) + hw, hw + 1 : imDimensions(2) + hw) = firstGrayMat;
 paddedSecondImage = zeros(imDimensions(1) + 2 * hw, imDimensions(2) + 2 * hw);
-paddedSecondImage(hw + 1 : imDimensions(1) + hw + 1, hw + 1 : imDimensions(2) + hw + 1) = secondGrayMat;
+paddedSecondImage(hw + 1 : imDimensions(1) + hw, hw + 1 : imDimensions(2) + hw) = secondGrayMat;
 paddedDepthMap = zeros(imDimensions + hw);
 
 %pick between ssd and normalized correlation
 ssd = true;
 
 %Basic algorithm: For each pixel scan along the epipolar line in the second
-%image and pick the lowest disparity.
+%image and pick the lowest dispRange.
 for column = 1 : imDimensions(2)
-   %find the interval of pixels to scan based on disparity range
+   %find the interval of columns to scan based on dispRange range
    scanStart = 0;
    scanEnd = 0;
-   if((column + (disparity/2) - 1) >= imDimensions(2))
+   if((column + (dispRange/2) - 1) >= imDimensions(2))
        scanEnd = imDimensions(2);
-       scanStart = imDimensions(2) - disparity + 1;
-   elseif ((column - disparity/2) < 1)
+       scanStart = imDimensions(2) - dispRange + 1;
+   elseif ((column - dispRange/2) < 1)
        scanStart = 1;
-       scanEnd = disparity;
+       scanEnd = dispRange;
    else
-       scanStart = column - (disparity/2);
-       scanEnd = scanStart + (disparity) - 1;
+       scanStart = column - (dispRange/2);
+       scanEnd = scanStart + (dispRange) - 1;
    end
-   scanStart = scanStart + windowSize;
-   scanEnd = scanEnd + windowSize;
    
-   %Get the source window from the left image;
-   
-   
+   for row = 1 : imDimensions(1)
+       %Get the source window from the left image;
+       paddedRow = row + hw;
+       paddedCol = column + hw;
+       leftWindow = GetWindow(paddedFirstImage, paddedRow, paddedCol, hw);
+       %Get an array of all windows to scan from right image
+       rightWindows = zeros(windowSize, windowSize, scanEnd - scanStart + 1);
+       rightWindowIndex = 1;
+       for scanCol = scanStart : scanEnd
+           paddedScanCol = scanCol + hw;
+           window = GetWindow(paddedSecondImage, paddedRow, paddedScanCol, hw);
+           rightWindows(:, :, rightWindowIndex) = window;
+           rightWindowIndex = rightWindowIndex + 1;
+       end
+   end
    
 end
